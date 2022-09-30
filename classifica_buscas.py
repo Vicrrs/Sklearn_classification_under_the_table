fbeb41ecac6b6fab90bbfd1ad384c36a00f0c435
Vicrrs <enricofermi2019@gmail.com>
2022-09-30 06:56:20 -0400

    commit project

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -7,3 +7,17 @@ Y_df = df['comprou']
 
 Xdummies_df = pd.get_dummies(X_df)
 Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+porcentagem_de_treino = 0.9
+
+tamanho_de_treino = int(porcentagem_de_treino * len(Y))
+tamanho_de_teste = len(Y) - tamanho_de_treino
+# 90% pra treinar
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+# 10% pra testar
+teste_dados = X[-tamanho_de_teste:]
+teste_marcacoes = Y[-tamanho_de_teste:]
